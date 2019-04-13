14011df5fdda3fb58eb747670b565627f9ddf8fd
Hanny <htw1127@gmail.com>
2019-04-13 22:54:53 +0900

    Completed first part of lab1git add lab1/*

---
+++ lab1/HelloNumber.java
@@ -0,0 +1,10 @@
+public class HelloNumber{
+	public static void main(String[] args) {
+		int sum = 0;
+		for(int x = 0; x < 10; x++){
+			sum += x;
+			System.out.print(sum + " ");
+		}
+		System.out.println();
+	}
+}
